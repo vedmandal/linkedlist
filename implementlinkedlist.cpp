f2f22b7ab506effbfed9ef511c8ce90386412107
Vedmandal <mandalved643@gmail.com>
2023-11-24 20:09:09 +0530

    implentation and display of linkedlist

---
+++ implementlinkedlist.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*next;
+    node(int val){
+        this->val=val;
+       this->next=NULL;
+    }
+};
+class linkedlist{
+
+node*head;
+node*tail;
+int s;
+public:
+linkedlist(){
+    head=NULL;
+    tail=NULL;
+    s=0;
+}
+
+void addlast(int val){
+    node*temp=new node(val);
+    if(s==0){
+        head=temp;
+        tail=temp;
+    }else{
+        tail->next=temp;
+        tail=tail->next;
+
+    }
+    s++;
+}
+void display(){
+    node*curr=head;
+    while(curr!=NULL){
+        cout<<curr->val<<" ";
+        curr=curr->next;
+    }
+}
+
+};
+int main(){
+    linkedlist a;
+    a.addlast(1);
+    a.addlast(2);
+    a.addlast(3);
+    a.addlast(4);
+    a.addlast(5);
+    a.display();
+}
+
+
+
